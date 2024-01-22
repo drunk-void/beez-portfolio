44a48e4d88da1183dc812cd5e257a39f07e83b03
Manish Chandolia <mchandolia35@gmail.com>
2024-01-22 17:42:55 +0530

    warning minor fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
 import Navbar from './components/Navbar/Navbar';
 import Projects from './components/Projects/Projects';
-import Skills from './components/Skills/Skills';
-import Testimonials from './components/Testimonials/Testimonials';
+// import Skills from './components/Skills/Skills';
+// import Testimonials from './components/Testimonials/Testimonials';
 
 function App() {
   return (
